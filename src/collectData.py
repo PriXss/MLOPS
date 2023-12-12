0e1f391314348513eda0c6dd5624271d7c90d872
Marcel Thomas <PriXss@users.noreply.github.com>
2023-12-12 18:06:54 +0100

    update collect function

--- src/collectData.py
+++ src/collectData.py
@@ -1,1 +1,15 @@
+import pandas as pd
+import os as os
+import csv as csv
+
+
+
 print("Datensatz wurde collected und kann im ML Prozess verwendet werden")
+
+data_path = os.path.join('data', 'collected')
+os.makedirs(data_path, exist_ok=True)
+
+df= pd.read_csv(r"./test.csv")
+
+print(df)
+
