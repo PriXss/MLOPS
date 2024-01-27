b7085fb118c615a9d01b8fcd3073b0f7a459b53c
Marcel Thomas <73349327+PriXss@users.noreply.github.com>
2024-01-27 22:14:26 +0100

    u

--- my-dagster-project/my_dagster_project/assets.py
+++ my-dagster-project/my_dagster_project/assets.py
@@ -1,23 +1,31 @@
 
+import os
+import csv
 from dagster import asset
-import boto3
-from botocore.config import Config
-import pandas as pd
 import subprocess
 
 @asset(group_name="data_collection", compute_kind="DataExtraction" )
-def getRawDataFromBucket() -> None:
-    s3 = boto3.resource(
-        endpoint_url= 'http://85.215.53.91:9000',
-        aws_secret_key_id= 'TqDMLOXxw2OipC6Tp0mv',
-        aws_secret_access_key='l1d0Elk1bH7X3bB2NOIXHOwGIJJSPcZvD9MLWQ58',
-        config= Config(signature_version='s3v4'),
-        region_name='us-east-1',
-    )
-    s3.Bucket('data').download_file('siemens.csv', 'siemens.csv')
+def callDataCollectionScript():
+    """This script is used to run the data collection script"""
+    # Replace 'path/to/directory' with the path where you want to create the directory
+    os.mkdir('/data/siemens/')
+
+    # Replace 'filename.csv' with the name of the CSV file you want to create
+    filename = 'siemens.csv'
+
+    # Replace 'path/to/directory' with the path of the directory you created earlier
+    filepath = os.path.join('/data/siemens/', filename)
+
+    # Create a new CSV file in the directory
+    with open(filepath, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['aktienName', 'Siemens'])
+        writer.writerow(['Preis', '100€'])
+
     
-@asset(deps=[getRawDataFromBucket] ,group_name="data_versioning", compute_kind="DataVersioning" )
+@asset(deps=[callDataCollectionScript] ,group_name="data_versioning", compute_kind="DataVersioning" )
 def version_with_DVC_Container() -> None:
+    subprocess.run(['cd', '/data/siemens/'])
     subprocess.run(['dvc', 'add', 'siemens.csv'])
     subprocess.run(['dvc', 'commit'])
     subprocess.run(['git', 'add' '.'])
