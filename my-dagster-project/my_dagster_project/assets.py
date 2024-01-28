f7728b6cde7debe2bce98e681cf6602bc091359f
Marcel Thomas <73349327+PriXss@users.noreply.github.com>
2024-01-28 15:27:39 +0100

    Update assets.py

--- my-dagster-project/my_dagster_project/assets.py
+++ my-dagster-project/my_dagster_project/assets.py
@@ -4,7 +4,7 @@ from dagster import asset
 import subprocess
 
 @asset(group_name="data_collection", compute_kind="DataExtraction" )
-def callDataCollectionScript():
+def callDataCollectionScript() -> None:
     """This script is used to run the data collection script"""
     # Replace 'path/to/directory' with the path where you want to create the directory
     os.mkdir('/data/siemens/')
@@ -23,9 +23,39 @@ def callDataCollectionScript():
 
     
 @asset(deps=[callDataCollectionScript] ,group_name="data_versioning", compute_kind="DataVersioning" )
-def version_with_DVC_Container() -> None:
-    subprocess.run(['cd', '/data/siemens/'])
+def version_with_DVC() -> None:
+    subprocess.run(['cd', 'data/siemens/'])
     subprocess.run(['dvc', 'add', 'siemens.csv'])
     subprocess.run(['dvc', 'commit'])
+    subprocess.run(['dvc', 'push'])
     subprocess.run(['git', 'add' '.'])
     subprocess.run(['git', 'commit', '-m', 'Added new siemens data'])
+
+
+@asset(group_name="ProvideModell", compute_kind="ModelServing" )
+def provideAndVersionMLModell() -> None:
+    # try to sping up the model container here or make a reference to the model that is beeing used
+    #track the model with dvbc as well, TBD how modell serving looks like
+    pass
+
+
+@asset(deps=[version_with_DVC, provideAndVersionMLModell ] ,group_name="DataToModell", compute_kind="DataProcessing" )
+def processDataFromDVCStorageWithModell() -> None:
+    subprocess.run(['dvc', 'pull'])
+    ##call the model container and pass the data to the container
+    ## recieve the results from ML modell and safe it as 
+    os.mkdir('/resultData')
+    filename = 'siemensResults.csv'
+
+    # Replace 'path/to/directory' with the path of the directory you created earlier
+    filepath = os.path.join('/resultData/', filename)
+    with open(filepath, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['Prognose', 'kaufen'])
+    ##store results in resultData as results.csv
+    subprocess.run(['dvc', 'add', 'resultData/siemensResults.csv'])
+    subprocess.run(['dvc', 'commit'])
+    subprocess.run(['dvc', 'push'])
+    subprocess.run(['git', 'add' '.'])
+    subprocess.run(['git', 'commit', '-m', 'Added siemens result data'])
+
