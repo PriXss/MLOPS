4cdbbf7d9e13e8620d6990cfb392dac9f07ecdd9
AlexHR00 <alexander.wallner@student.reutlingen-university.de>
2024-02-11 17:36:34 +0100

    Implementierung von Bucket-Daten direkt im Code

--- Docker_Train_Model/docker_train_ludwig_model.py
+++ Docker_Train_Model/docker_train_ludwig_model.py
@@ -3,37 +3,31 @@ import pandas as pd
 from ludwig.api import LudwigModel
 import configparser
 import boto3
-
+from io import StringIO
 
 class MLFlowTrainer:
-    def __init__(self, ludwig_config_path, dataset_path, model_name="deep_lstm"):
+    def __init__(self, ludwig_config_path, model_bucket_url, dataset_path, model_name="deep_lstm"):
         self.ludwig_config_path = ludwig_config_path
+        self.model_bucket_url = model_bucket_url
         self.dataset_path = dataset_path
         self.model_name = model_name
 
-        # Laden der Bucket-Informationen aus der Konfigurationsdatei
-        self.config = configparser.ConfigParser()
-        self.config.read('Dataconfig')
-
         # Setzen der Bucket-URLs
-        self.data_bucket_url = self.config['remote "minio"']['url']
-        self.access_key_id = self.config['remote "minio"']['access_key_id']
-        self.secret_access_key = self.config['remote "minio"']['secret_access_key']
-        self.endpoint_url = self.config['remote "minio"']['endpointurl']
+        self.data_bucket_url = "s3://data"
+        self.access_key_id = "test"
+        self.secret_access_key = "testpassword"
+        self.endpoint_url = "http://85.215.53.91:9000"
 
         # Setzen der Umgebungsvariablen für den Zugriff auf Buckets
         os.environ["AWS_ACCESS_KEY_ID"] = self.access_key_id
         os.environ["AWS_SECRET_ACCESS_KEY"] = self.secret_access_key
         os.environ["AWS_ENDPOINT_URL"] = self.endpoint_url
 
-        #Lade die modelConfig
-        self.config = configparser.ConfigParser()
-        self.config.read('config')
-        self.model_bucket_url = self.config['remote "minio"']['url']
-
     def train_model(self):
         # Laden der Daten aus dem Bucket
-        data = pd.read_csv(self.data_bucket_url)
+        s3 = boto3.client('s3')
+        obj = s3.get_object(Bucket=self.data_bucket_url, Key='data.csv')
+        data = pd.read_csv(obj['Body'])
 
         # Trainieren des Ludwig-Modells
         # Aufteilung der Daten(train_set, test_set, validation_set)
@@ -55,7 +49,8 @@ if __name__ == "__main__":
     ludwig_config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../ludwig_MLCore.yaml")
     model_name = "Test_Model"
     dataset_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../data/data.csv")
+    model_bucket_url = "s3://data"
 
     # Instanz der Klasse erstellen und das Modell trainieren
-    trainer = MLFlowTrainer(ludwig_config_path, model_name)
+    trainer = MLFlowTrainer(ludwig_config_path, model_bucket_url, dataset_path, model_name)
     trainer.train_model()
