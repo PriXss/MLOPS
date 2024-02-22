65fc7ad1410cd99080c2a3c7c10971af2c83b9b9
Marcel Thomas <PriXss@users.noreply.github.com>
2024-02-22 08:19:14 +0100

    last commit

--- my-dagster-project/quickstart_etl/assets/prod-pipeline.py
+++ my-dagster-project/quickstart_etl/assets/prod-pipeline.py
@@ -14,7 +14,11 @@ from alpha_vantage.techindicators import TechIndicators
 from botocore.exceptions import NoCredentialsError
 import requests
 import json
-
+from evidently.report import Report
+from evidently.metric_preset import DataDriftPreset, TargetDriftPreset, DataQualityPreset, RegressionPreset
+from evidently.metrics import *
+from evidently.tests import *
+import warnings
 
 class MLFlowTrainer:
     def __init__(self, model_bucket_url, model_name="", ludwig_config_file_name="", data_file_name= ""):
@@ -359,11 +363,44 @@ def versionPrediction(context) -> None:
     context.log.info('Prediction successfully versioned')
 
 
-@asset(deps=[requestToModel], group_name="VersioningPhase", compute_kind="DVCDataVersioning")
-def versioningModel(context) -> None:
-    context.log.info('I can identify which model is served atm and have a reference to it accordingly')
-
-
-@asset(deps=[versioningModel, versionPrediction, versionStockData], group_name="MonitoringPhase", compute_kind="Reporting")
+@asset(deps=[versionPrediction, versionStockData], group_name="MonitoringPhase", compute_kind="Reporting")
 def monitoringAndReporting(context) -> None:
-    context.log.info('Monitoring part')
+    
+    ##### Ignore warnings #####
+    warnings.filterwarnings('ignore')
+    warnings.simplefilter('ignore')
+
+    ##### Set file/bucket vars #####
+    data_bucket_url = "logs"
+    data_stock = "IBM"
+    data_model_version = "model2"
+    data_date = "20022024"
+
+    ##### Set access vars #####
+    os.environ["AWS_ACCESS_KEY_ID"] = "test"
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "testpassword"
+    os.environ["AWS_ENDPOINT_URL"] = "http://85.215.53.91:9000"
+
+    ##### Load data from the bucket #####
+    s3 = boto3.client('s3')
+    obj = s3.get_object(Bucket=data_bucket_url, Key= data_date+"_"+data_stock+"_"+data_model_version+".csv" )
+    df = pd.read_csv(obj['Body'])
+
+    ##### Create report #####
+    reference = df.iloc[50:,:]
+    current = df.iloc[:50,:]
+
+    report = Report(metrics=[
+        DataDriftPreset(), 
+        TargetDriftPreset(),
+        RegressionPreset()
+    ])
+
+    report.run(reference_data=reference, current_data=current)
+    report.save_html("report.html")
+
+    ##### Upload report to bucket #####
+    s3 = boto3.resource('s3')
+    obj = s3.Object("reports", "/"+data_stock+"/"+data_model_version+"/report.html")
+    obj.put(Body=open('report.html', 'rb'))
+    context.log.info('Monitoring part running')
