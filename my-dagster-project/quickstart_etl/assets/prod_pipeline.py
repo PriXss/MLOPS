ea21eab5c024ac164a91d73b8fbada9c95ba7dba
Marcel Thomas <73349327+PriXss@users.noreply.github.com>
2024-05-05 11:43:59 +0200

    small changes

--- my-dagster-project/quickstart_etl/assets/prod_pipeline.py
+++ my-dagster-project/quickstart_etl/assets/prod_pipeline.py
@@ -394,7 +394,7 @@ def setupDVCandVersioningBucket(context) -> None:
     subprocess.run(["dvc", "push"])
     subprocess.run(["git", "add", "."])
     subprocess.run(["git", "commit", "-m", "Add new DVC Config for todays run"])
-    subprocess.run(["git", "push" "origin" "HEAD:origin/DagsterPipelineProdRun"])
+    subprocess.run(["git", "push" "-u" "origin" "DagsterPipelineProdRun"])
     
 
 
