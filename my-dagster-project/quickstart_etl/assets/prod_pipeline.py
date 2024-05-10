e9e528f840c7ca4ba0b43db7d282b6e030bdce9a
Marcel Thomas <PriXss@users.noreply.github.com>
2024-05-10 16:08:58 +0200

    Update prod_pipeline.py

--- my-dagster-project/quickstart_etl/assets/prod_pipeline.py
+++ my-dagster-project/quickstart_etl/assets/prod_pipeline.py
@@ -150,7 +150,7 @@ def process_and_upload_symbol_data(
                 print('Zugriffsberechtigungsfehler. Stellen Sie sicher, dass Ihre Minio S3-Zugriffsdaten korrekt sind.')
             except Exception as e:
                 print(f'Ein Fehler ist aufgetreten: {str(e)}')
-        subprocess.call("git", "add", f"{output_directory}/{csv_filename}", shell=True) 
+        subprocess.run("git", "add", f"{output_directory}/{csv_filename}", shell=True) 
 
 
 session = boto3.session.Session()
