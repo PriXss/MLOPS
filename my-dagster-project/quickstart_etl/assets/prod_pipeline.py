c43e639ef1f8ee85b0e2cb419a309dbf886d1d0a
GlennVerhaag <gaver1998@gmail.com>
2024-05-15 16:11:01 +0200

    Removed unused bucket reference

--- my-dagster-project/quickstart_etl/assets/prod_pipeline.py
+++ my-dagster-project/quickstart_etl/assets/prod_pipeline.py
@@ -513,7 +513,7 @@ def setupDVCandVersioningBucket(context) -> None:
     timestamp=timestampTemp
     
     # Check if all required buckets for the pipeline exist. If not, create them.
-    buckets = [os.getenv("OUTPUT_DIRECTORY"), os.getenv("PREDICTIONS_BUCKET"), os.getenv("MODEL_BUCKET"), os.getenv("MLFLOW_BUCKET"), os.getenv("MODEL_CONFIG_BUCKET"), os.getenv("LOGS_BUCKET"), os.getenv("VERSIONING_BUCKET"), os.getenv("VERSIONING_TRAINING_BUCKET")]
+    buckets = [os.getenv("OUTPUT_DIRECTORY"), os.getenv("PREDICTIONS_BUCKET"), os.getenv("MODEL_BUCKET"), os.getenv("MLFLOW_BUCKET"), os.getenv("MODEL_CONFIG_BUCKET"), os.getenv("VERSIONING_BUCKET"), os.getenv("VERSIONING_TRAINING_BUCKET")]
     for bucket in buckets:
         
         if check_bucket_exists(bucket):
