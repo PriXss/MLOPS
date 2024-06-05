ba9d5bb069091a4d4c124b3a98a23664a0a88faf
VinuelMancent <vincentmattes@gmail.com>
2024-06-05 17:08:39 +0000

    added port env var and made container run in daemon mode

--- serve_script_files/serve_script.sh
+++ serve_script_files/serve_script.sh
@@ -3,4 +3,4 @@ python3 download_boto3_new_key.py
 imagename=$(echo "$MODEL_NAME" | tr '[:upper:]' '[:lower:]')
 echo "$imagename"
 sudo docker build --build-arg model_name=${imagename} -t ${imagename} .
-sudo docker run -it -p 8001:8000 ${imagename}
+sudo docker run -d -it -p ${port}:8000 ${imagename}
